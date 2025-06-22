98405f46bc2f8cc90fa2f17cdf3212b1c5984aca
haidy <ren.hong@northeastern.edu>
2025-06-22 12:29:36 -0700

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <HashRouter>
       <Provider store={store}>
         <Routes>
-          <Route path="/" element={<Navigate to="/Kambaz" />} />
+          <Route path="/" element={<Navigate to="/Kambaz/GitHub" />} />
           <Route path="/Kambaz/*" element={<Kambaz />} />
         </Routes>
       </Provider>
